9f8a979ad7db1adc705396691a1dc786157653ef
CroiXan <xan.mark.vi@gmail.com>
2024-08-15 22:04:51 -0400

    login screen

--- app/src/main/java/com/example/talkcompanion/MainActivity.kt
+++ app/src/main/java/com/example/talkcompanion/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.talkcompanion.components.LoginScreen
 import com.example.talkcompanion.ui.theme.TalkCompanionTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,6 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TalkCompanionTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LoginScreen()
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
---
+++ app/src/main/java/com/example/talkcompanion/components/LoginScreen.kt
@@ -0,0 +1,75 @@
+package com.example.talkcompanion.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LoginScreen() {
+    var email by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16. dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Login", fontSize = 24. sp)
+
+        TextField(
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            label = {
+                Text("Email")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8. dp)
+        )
+
+        TextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            label = {
+                Text("Password")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8. dp),
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Button(
+            onClick = {
+                /* Handle login logic here */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8. dp)
+        ) {
+            Text(text = "Login")
+        }
+    }
+}
