3c4b81a5a6c5bfbbe369cc944eeb6b11e4159281
CroiXan <xan.mark.vi@gmail.com>
2024-09-04 23:56:53 -0400

    phrase management logic

--- app/src/main/java/com/example/talkcompanion/data/functions/PhraseFunctions.kt
+++ app/src/main/java/com/example/talkcompanion/data/functions/PhraseFunctions.kt
@@ -2,14 +2,17 @@ package com.example.talkcompanion.data.functions
 
 import android.content.Context
 import com.example.talkcompanion.data.model.Phrase
+import com.example.talkcompanion.feature.login.functions.getCurrentUserName
 import com.example.talkcompanion.feature.usermanagement.functions.getPhraseListMock
+import com.example.talkcompanion.feature.usermanagement.functions.savePhraseListMock
 import com.google.gson.Gson
 
-fun getPhraseListByUserName(context: Context, userName: String){
-    
+fun getPhraseListByUserName(context: Context): List<Phrase> {
+    val userName = getCurrentUserName(context, "currentUserName")
+    return getPhraseList(context).filter { it.userName == userName }.sortedBy { it.orderNumer }
 }
 
-fun getPhraseList(context: Context): ArrayList<Phrase> {
+private fun getPhraseList(context: Context): ArrayList<Phrase> {
     val gson = Gson()
     val json = getPhraseListMock(context)
     if(json != null){
@@ -18,6 +21,12 @@ fun getPhraseList(context: Context): ArrayList<Phrase> {
     return ArrayList<Phrase>()
 }
 
-fun updatePhraseList(){
-
+fun addPhraseList(context: Context, newPhrase: String){
+    val userName = getCurrentUserName(context, "currentUserName")?:""
+    val userPhraseList = getPhraseListByUserName(context)
+    val phraseList = getPhraseList(context)
+    val maxId = phraseList.maxBy { it.id }.id
+    val newPhraseItem = Phrase(maxId + 1, userName, newPhrase, userPhraseList.size + 1)
+    phraseList.add(newPhraseItem)
+    savePhraseListMock(context, Gson().toJson(phraseList))
 }
